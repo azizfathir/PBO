32fa14ff539d3d4264863a69120b6889d25f3d3b
azizfathir <azizzfathirr@gmail.com>
2025-10-08 15:21:49 +0700

    Add files via upload

---
+++ Class.java
@@ -0,0 +1,25 @@
+//import java.util.Scanner;
+//
+//public class Ak47 {
+//    public static void main (String[]args){
+//        Scanner input = new Scanner(System.in);
+//        int peluru = 7 ;
+//        int tembak ;
+//        do{
+//            System.out.println("ketik 1 untuk menembak :");
+//            tembak = input.nextInt();
+//
+//            if (tembak==1){
+//                peluru -- ;
+//                System.out.println("DOR ! Sisa Satu peluru:" + peluru);
+//            }else{
+//                System.out.println("input salah ketik 1 untuk menembak:");
+//
+//                }
+//            }
+//        while(peluru > 0);
+//        System.out.println("peluru Habis");
+//        input.close();
+//        }
+//
+//    }
---
+++ Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        //---Aritmatika---
+       int pilihan;
+//        int a = a + b;
+//        int b = a - b;
+//        int c = a * b;
+//        int d = a / b;
+        System.out.println("Masukkan Angka Pertama");
+        System.out.println("Masukkan Angka Kedua");
+
+        System.out.println("a + b =");
+        System.out.println("a - b =");
+        System.out.println("a * b =");
+        System.out.println("a / b =");I
+        switch (pilihan) {
+            case 1:
+                System.out.println("a + b = ");
+                System.out.println("Tambah");
+                break;
+            case 2:
+                System.out.println("a - b = ");
+                System.out.println("kurang");
+                break;
+            case 3:
+                System.out.println("a * b =");
+                System.out.println("kali");
+                break;
+            case 4:
+                System.out.println("a / b =");
+                System.out.println("bagi");
+                break;
+        }
+
+    }
+
+}
